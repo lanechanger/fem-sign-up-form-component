242daea97581be6a6ac0a5062d8c8cf305740fa2
lanechanger <jeremy_ng_86@live.ca>
2021-05-08 23:45:32 -0400

    feat: styled the form and begun styling error state

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+console.log("Hello World");
+const firstName = document.querySelector("#first-name");
+const lastName = document.querySelector("#last-name");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+
+const firstError = document.querySelector(".register--first-error");
+const lastError = document.querySelector(".register--last-error");
+const emailError = document.querySelector(".register--email-error");
+const passError = document.querySelector(".register--pass-error");
+
+const registerBtn = document.querySelector(".register__btn");
+
+registerBtn.addEventListener("click", function () {
+  console.log("clicked register");
+
+  let firstValue = firstName.value;
+  let lastValue = lastName.value;
+  let emailValue = email.value;
+  let passValue = password.value;
+
+
+});
