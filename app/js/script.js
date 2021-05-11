c28317ba554f322e6709c70f3cdc2301ece5c020
lanechanger <jeremy_ng_86@live.ca>
2021-05-10 23:34:01 -0400

    feat: added is-error class to turn on error messages

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,7 @@
-console.log("Hello World");
-const firstName = document.querySelector("#first-name");
-const lastName = document.querySelector("#last-name");
-const email = document.querySelector("#email");
-const password = document.querySelector("#password");
+const firstInput = document.querySelector("#first-name");
+const lastInput = document.querySelector("#last-name");
+const emailInput = document.querySelector("#email");
+const passInput = document.querySelector("#password");
 
 const firstError = document.querySelector(".register--first-error");
 const lastError = document.querySelector(".register--last-error");
@@ -10,14 +9,40 @@ const emailError = document.querySelector(".register--email-error");
 const passError = document.querySelector(".register--pass-error");
 
 const registerBtn = document.querySelector(".register__btn");
+const validEmailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-registerBtn.addEventListener("click", function () {
+registerBtn.addEventListener("click", function (event) {
+  event.preventDefault();
   console.log("clicked register");
 
-  let firstValue = firstName.value;
-  let lastValue = lastName.value;
-  let emailValue = email.value;
-  let passValue = password.value;
+  let firstValue = firstInput.value;
+  let lastValue = lastInput.value;
+  let emailValue = emailInput.value;
+  let passValue = passInput.value;
 
-
+  validateEmpty(firstValue, firstInput, firstError);
+  validateEmpty(lastValue, lastInput, lastError);
+  validateEmail(emailValue, emailInput, emailError);
+  validateEmpty(passValue, passInput, passError);
 });
+
+function validateEmpty(value, input, error) {
+  if (value == "") {
+    // add error state
+    console.log("value is empty, adding error state");
+    error.classList.add("is-error");
+  } else {
+    // remove error state
+    console.log("value is good, removing error state");
+  }
+}
+
+function validateEmail(value, input, error) {
+  if (value.match(validEmailRegex)) {
+    // remove error state
+    console.log("valid email, removing error state");
+  } else {
+    // add error state
+    console.log("invalid email, adding error state");
+  }
+}
