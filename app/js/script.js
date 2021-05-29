7091f6eaa82f6f5c558465a6753a1771294f516f
lanechanger <jeremy_ng_86@live.ca>
2021-05-29 00:40:07 -0400

    feat: redid the scripting to include inline validation for the inputs

--- app/js/script.js
+++ app/js/script.js
@@ -3,39 +3,57 @@ const lastInput = document.querySelector("#last-name");
 const emailInput = document.querySelector("#email");
 const passInput = document.querySelector("#password");
 
-const firstError = document.querySelector(".register--first-error");
-const lastError = document.querySelector(".register--last-error");
-const emailError = document.querySelector(".register--email-error");
-const passError = document.querySelector(".register--pass-error");
+const firstError = document.querySelector("#firstError");
+const lastError = document.querySelector("#lastError");
+const emailError = document.querySelector("#emailError");
+const passError = document.querySelector("#passError");
 
-const registerBtn = document.querySelector(".register__btn");
+const registerBtn = document.querySelector("#freeTrialBtn");
 
 const inputIsErrorClass = "register__input--is-error";
 const errorIsErrorClass = "is-error";
 
 const validEmailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+let firstValue, lastValue, emailValue, passValue;
+
+firstInput.addEventListener("blur", function () {
+  console.log("inline validation for first name");
+  validateEmpty(firstInput, firstError);
+});
+
+lastInput.addEventListener("blur", function () {
+  console.log("inline validation for last name");
+  validateEmpty(lastInput, lastError);
+});
+
+emailInput.addEventListener("blur", function () {
+  console.log("inline validation for email");
+  validateEmail(emailInput, emailError);
+});
+
+passInput.addEventListener("blur", function () {
+  console.log("inline validation for password");
+  validateEmpty(passInput, passError);
+});
+
 registerBtn.addEventListener("click", function (event) {
   event.preventDefault();
   console.log("clicked register");
 
-  let firstValue = firstInput.value;
-  let lastValue = lastInput.value;
-  let emailValue = emailInput.value;
-  let passValue = passInput.value;
-
-  if (validateEmpty(firstValue, firstInput, firstError) &
-    validateEmpty(lastValue, lastInput, lastError) &
-    validateEmail(emailValue, emailInput, emailError) &
-    validateEmpty(passValue, passInput, passError)) {
+  if (validateEmpty(firstInput, firstError) &
+    validateEmpty(lastInput, lastError) &
+    validateEmail(emailInput, emailError) &
+    validateEmpty(passInput, passError)) {
     // submit form only if all inputs validate
     console.log("all 4 inputs valid, submitting");
     document.querySelector(".register").submit();
   }
 });
 
-function validateEmpty(value, input, error) {
-  if (value == "") {
+
+function validateEmpty(input, error) {
+  if (input.value == "") {
     // add error state
     console.log("value is empty, adding error state");
     addErrorClasses(input, error);
@@ -48,8 +66,8 @@ function validateEmpty(value, input, error) {
   }
 }
 
-function validateEmail(value, input, error) {
-  if (value.match(validEmailRegex)) {
+function validateEmail(input, error) {
+  if (input.value.match(validEmailRegex)) {
     // remove error state
     console.log("valid email, removing error state");
     removeErrorClasses(input, error);
